29bbbde8b630bd9b18cffc454b44a56dd7df978d
Greg Pfaff <greg.pfaff@razorfish.com>
2019-07-19 09:44:57 -0500

    Updates to Mobile

--- src/js/main/map.js
+++ src/js/main/map.js
@@ -2,5 +2,5 @@ function initMap() {
   var hydePark = {lat: 43.097672, lng: -79.020234};
   var map = new google.maps.Map(
     document.getElementById('map'), { zoom: 14, center: hydePark});
-  var marker = new google.maps.Marker({ position: hydePark, map});
+  new google.maps.Marker({ position: hydePark, map});
 }
