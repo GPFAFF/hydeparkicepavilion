078ea9208413712ebfb9db64a1294ffb9303fb05
Greg Pfaff <greg.pfaff@silvercar.com>
2019-07-22 07:47:18 -0500

    fix button container

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,16 +21,14 @@ const chevron = document.querySelector('.chevron');
 
 chevron.addEventListener('click', (event) => {
   event.preventDefault();
-  console.log('click');
   if (!event.target.className.includes('chevron')) return;
-  console.log('chevron')
   const info = document.querySelector('.rink-info');
   singleton(info, {
     duration: 1000,
     offset: 0,
     callback: undefined,
     easing: easeInOutQuad,
-    a11y: false
+    a11y: true
   })
 })
 function initMap() {
